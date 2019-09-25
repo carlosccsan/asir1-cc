b69abf7bb495269341badeb384b5e4982b6a78d6
carlosccsan <carlosccsan@icloud.com>
2019-09-25 08:51:17 +0200

    version10

--- asir1.php
+++ asir1.php
@@ -1,5 +1,5 @@
 <?php
-echo "<a href='http://192.168.0.84'>Rolando</a>";
+echo "<a href='http://192.168.0.84/Asir1-RMH/asir1.php'>Rolando</a>";
 $f=fopen('visitas.txt','a');
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
 fclose($f)
