3c0c5d26e49ccfb78fc593e852ca3c92183c1971
carlosccsan <carlosccsan@icloud.com>
2019-09-25 10:33:05 +0200

    version11.suscriptores.con.hora

---
+++ asir1.php
@@ -0,0 +1,15 @@
+
+<?php
+echo '<a href="http://192.168.0.84/asir1-RMH/asir1.php">Página de Rolando</a>';
+echo "<br>";
+echo "Aarón Martín López";
+echo "<br>";
+echo '<a href="http://192.168.0.191">Página de Alex</a>';
+  $archivo="visitas.txt";
+  $proceso=fopen($archivo,"a") or die("basura");
+  $ip=$_SERVER['REMOTE_ADDR'];
+  $hoy = date('Y-m-d H:i:s');
+  $datos="La ip es:".$ip." y fecha: ".$hoy."\r\n";
+  fwrite($proceso,$datos);
+  fclose($proceso);
+?>
