bf7f89641efde4d596d7e6fd2d7ceb6fd112f052
carlosccsan <carlosccsan@icloud.com>
2019-10-02 09:25:08 +0200

    version19

--- 2e.php
+++ 2e.php
@@ -1,24 +1,17 @@
- <table border="1" align="center">
-            <?php
-            echo "<tr>";
-            for ($cabecera="0";$cabecera<=10;$cabecera++){
-                        echo "<th>";
-                        echo $cabecera;
-                        echo "</th>";
-            }
-            echo "</tr>";
-            for ($x = 1; $x <=10; $x++){
-                       echo "<tr>";
-                       echo "<th>";
-                       echo $x;
-                       echo "</th>";
-                            for ($y=1;$y<=10;$y++){                      
-                                    $multiplicacion=$x*$y;                    
-                                    echo "<td>";                      
-                                    echo $multiplicacion;                      
-                                    echo "</td>";              
-                            }              
-                    echo "</tr>";          
-                    }      
-    ?>      
-    </table>
+<?php
+for($n=1;$n<11;$n++){
+	echo '<div style="
+		float:left;
+		margin:20px;
+		background:pink;
+	">';
+	for($i=1;$i<=10;$i=$i+1){
+		echo $n;
+		echo ' * ';
+		echo $i;
+		echo ' = ';
+		echo $n*$i;
+		echo '<br/>';
+	}
+	echo '</div>';
+}
